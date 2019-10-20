552c87bccb57ce2dd26e30ecd41f14658333a941
tylurp <tylurp1@gmail.com>
2019-10-20 12:34:38 -0700

    add alert tests

--- Tests/cliTests/cliTests.swift
+++ Tests/cliTests/cliTests.swift
@@ -8,6 +8,19 @@ final class cliTests: XCTestCase {
         // results.
         XCTAssertEqual(Symbols.tick, "✔")
     }
+    
+    func testAlerts() {
+        let success_alert = Symbols.tick.green() + " " + "Success"
+        let failure_alert = Symbols.cross.red() + " " + "Failure"
+        let info_alert = Symbols.info + " " + "Info"
+        let warning_alert = "!".bold().red() + " " + "Warning"
+        let generic_alert = Symbols.arrow_right + " " + "Generic"
+        XCTAssertEqual(AlertSuccess(), success_alert)
+        XCTAssertEqual(AlertFailure(), failure_alert)
+        XCTAssertEqual(AlertInfo(), info_alert)
+        XCTAssertEqual(AlertWarning(), warning_alert)
+        XCTAssertEqual(AlertGeneric(), generic_alert)
+    }
 
     static var allTests = [
         ("testExample", testExample),
